98102bc1d8de1f733cb049b18b2f6424ccbbd61b
ikhanter <khantermail@mail.ru>
2024-04-23 09:40:58 +0300

    ...

--- referral/views.py
+++ referral/views.py
@@ -12,7 +12,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from referral.backend import PhoneNumberBackend
 from referral.models import User, ConfirmationCode, ReferredUsers
