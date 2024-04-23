dfd9aeea483719d11c1db9a75c57c1ca05338de8
ikhanter <khantermail@mail.ru>
2024-04-23 09:35:07 +0300

    ...

--- hammer_test_case/settings.py
+++ hammer_test_case/settings.py
@@ -158,7 +158,6 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
